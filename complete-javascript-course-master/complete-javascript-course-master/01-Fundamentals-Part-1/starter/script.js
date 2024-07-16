bb292c71ffd08580e1fae1f99bbcb4a4cad29179
Eyob <eyobhg3@gmail.com>
2024-07-16 13:52:40 +0300

    Add datatype exercises

---
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,21 @@
+// alert("mango");
+
+let country = "Ethiopia";
+let continent = "Africa";
+let population = "1.23 Million";
+
+console.log(country, continent, population)
+
+
+let mango;
+console.log(typeof mango)
+console.log(mango)
+
+let num = 3;
+console.log(typeof num);
+
+let dynamic = true;
+console.log(typeof dynamic);
+
+dynamic = "JavaScript"
+console.log(typeof dynamic);
