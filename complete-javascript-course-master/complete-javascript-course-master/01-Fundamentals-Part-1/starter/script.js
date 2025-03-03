30021c5415b536175aae651c49bc915b700bcdf7
Developer <eyob.hg08@gmail.com>
2025-03-03 17:01:47 +0300

    Add javascript fundamentals practice

--- complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,21 +1,39 @@
+// Linking a JS file
 // alert("mango");
 
-let country = "Ethiopia";
-let continent = "Africa";
-let population = "1.23 Million";
+// let country = "Ethiopia";
+// let continent = "Africa";
+// let population = "1.23 Million";
 
-console.log(country, continent, population)
+// console.log(country, continent, population)
+10 * 2 + 99 - 6;
+console.log(10 * 2 + 99 - 9);
+
+// Data types
+
+// let mango;
+// console.log(typeof mango)
+// console.log(mango)
+
+// let num = 3;
+// console.log(typeof num);
+
+// let dynamic = true;
+// console.log(typeof dynamic);
+
+// dynamic = "JavaScript"
+// console.log(typeof dynamic);
 
 
-let mango;
-console.log(typeof mango)
-console.log(mango)
+// Let, const, and var
+// const birthYear = 2000;
+// console.log(birthYear);
 
-let num = 3;
-console.log(typeof num);
 
-let dynamic = true;
-console.log(typeof dynamic);
+// let age = 23;
+// age = 24;
+// console.log(age);
 
-dynamic = "JavaScript"
-console.log(typeof dynamic);
+
+// Basic operators
+// Operator precedence
