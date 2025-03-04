c976971ebbcac378df220c7719a6351e110f9b04
Developer <eyob.hg08@gmail.com>
2025-03-04 16:54:22 +0300

    Add assignment 1,2,3, 4, and Project 1.

---
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/BMI_Assignment.js
@@ -0,0 +1,15 @@
+// Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
+let massMark = 78;
+let heightMark = 1.69;
+
+let massJohn = 92;
+let heightJohn = 1.95;
+
+let BMIMark = massMark / (heightMark * heightMark);
+let BMIJohn = massJohn / (heightJohn * heightJohn);
+
+console.log("John's BMI = " + BMIJohn);
+console.log("Mark's BMI = " + BMIMark);
+
+let markHigherBMI = BMIMark > BMIJohn;
+console.log("Mark's BMI is higher? " + markHigherBMI);
--- complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,29 +1,51 @@
 // Linking a JS file
 // alert("mango");
 
-// let country = "Ethiopia";
-// let continent = "Africa";
-// let population = "1.23 Million";
+// 10 * 2 + 99 - 6;
+// console.log(10 * 2 + 99 - 9);
 
-// console.log(country, continent, population)
-10 * 2 + 99 - 6;
-console.log(10 * 2 + 99 - 9);
+// Values and Variables 
+// Assignments
+let country = "Ethiopia";
+let continent = "Africa";
+let population = 134353289;
+// let isIsland = false;
+// let language = "Ge'ez";
+const isIsland = false;
+const language = "Ge'ez";
+// language = 'Amharic'; // Assignment to constant variable.
+
+console.log("Country = ", country);
+console.log("Continent = ", continent);
+console.log("Population = ", population);
+console.log("Island = ", isIsland);
+console.log("Language = ", language);
+
+let halfPopulation = population / 2;
+console.log("Half Population = ", halfPopulation);
+console.log("Population Increment = ", population++);
+console.log("Population Increment = ", ++population);
+console.log("Does " + country + " have more people than Norway? " + (population > 6000000));
+
+
 
 // Data types
+/*
+let mango;
+console.log(typeof mango)
+console.log(mango)
 
-// let mango;
-// console.log(typeof mango)
-// console.log(mango)
+let theType = typeof mango;
+alert(theType);
+let num = 3;
+console.log(typeof num);
 
-// let num = 3;
-// console.log(typeof num);
+let dynamic = true;
+console.log(typeof dynamic);
 
-// let dynamic = true;
-// console.log(typeof dynamic);
-
-// dynamic = "JavaScript"
-// console.log(typeof dynamic);
-
+dynamic = "JavaScript"
+console.log(typeof dynamic);
+*/
 
 // Let, const, and var
 // const birthYear = 2000;
@@ -34,6 +56,10 @@ console.log(10 * 2 + 99 - 9);
 // age = 24;
 // console.log(age);
 
+// Missing initializer in const declaration
+/*const height;
+height = 1.89;
+*/
 
 // Basic operators
 // Operator precedence
