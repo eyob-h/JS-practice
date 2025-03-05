c165643a9f1ad317b076a86262932a9d02c78a46
Developer <eyob.hg08@gmail.com>
2025-03-05 23:20:09 +0300

    Add type coercion exercise

--- complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -80,3 +80,8 @@ string which was written
 using template literals!🐱‍🏍`;
 console.log(multiLine);
 
+console.log('9' - '5'); // -> 4
+console.log('19' - '13' + '17'); // -> 617
+console.log('19' - '13' + 17); // -> 23
+console.log('123' < 57); // -> false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // -> 117x  1143      
