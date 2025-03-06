1b7bbda4ebb7fbc18e7dc0b59ea78fa1e1fda0d0
eyob-h <eyobhg3@gmail.com>
2025-03-06 16:35:39 +0300

    Add solution to challenge 3

---
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/Dolphins and Koalas.js
@@ -0,0 +1,10 @@
+let scoreDolphins = (96 + 108 + 89) / 3;
+let scoreKoalas = (88 + 91 + 110) / 3;
+
+if (scoreDolphins > scoreKoalas) {
+    console.log(`Dolphins 🐬 win the trophy🏆`);
+} else if (scoreDolphins < scoreKoalas) {
+    console.log(`Koalas 🐨 win the trophy🏆`);
+} else if (scoreDolphins === scoreKoalas) {
+    console.log(`Both win the trophy`)
+}
